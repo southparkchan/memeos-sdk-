4d8c30d708c5d2887a18774387ebda897607bdc3
sparkchan <zaenxbt@gmail.com>
2025-11-15 03:32:43 +0700

    Update memeVM.ts

--- src/memeVM.ts
+++ src/memeVM.ts
@@ -1,1 +1,20 @@
+import { randomId } from "./utils/id";
+import { MemeInput, MemeRecord } from "./types/meme";
+
+export const memeVM = {
+  createMeme: (input: MemeInput): MemeRecord => {
+    return {
+      id: randomId(),
+      createdAt: Date.now(),
+      ...input
+    };
+  },
+
+  amplify: (meme: MemeRecord, power = 1): MemeRecord => {
+    return {
+      ...meme,
+      power: meme.power + power
+    };
+  }
+};
 
