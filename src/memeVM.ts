458cffa3e4cb976d87dbef2ebaeab9dd3fc56540
sparkchan <zaenxbt@gmail.com>
2025-11-15 03:33:25 +0700

    Update memeVM.ts

--- src/memeVM.ts
+++ src/memeVM.ts
@@ -17,4 +17,14 @@ export const memeVM = {
     };
   }
 };
+export interface MemeInput {
+  prompt: string;
+  power?: number;
+  tags?: string[];
+}
+
+export interface MemeRecord extends MemeInput {
+  id: string;
+  createdAt: number;
+}
 
