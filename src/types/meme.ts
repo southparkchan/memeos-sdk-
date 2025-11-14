3f924992cd2ee07d30d7dfd0428fdb8cd47c756c
sparkchan <zaenxbt@gmail.com>
2025-11-15 03:38:07 +0700

    Update meme.ts

--- src/types/meme.ts
+++ src/types/meme.ts
@@ -1,1 +1,11 @@
+export interface MemeInput {
+  prompt: string;
+  power?: number;
+  tags?: string[];
+}
+
+export interface MemeRecord extends MemeInput {
+  id: string;
+  createdAt: number;
+}
 
